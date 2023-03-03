9f53eed7ad4fa60860fc57f7472d9545e31654a0
JuanAntonioMar <126912392+JuanAntonioMar@users.noreply.github.com>
2023-03-03 20:44:10 +0100

    Add files via upload

---
+++ JuanAntonioMarinBuenoGIT.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.juanantoniomarinbueno.git;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JuanAntonioMarinBuenoGIT {
+
+    public static void main(String[] args) {
+        System.out.println("Me presento");
+        String nombre= "Juan";
+        String apellido= "Marín";
+        String apellido2= "Bueno";
+        String edad="24";
+        String altura="1.82cm";
+        System.out.println(nombre + " " + apellido + " " + apellido2 + " " + edad + " " + altura);
+        
+        
+        
+        
+        
+        
+        
+    }
+}
